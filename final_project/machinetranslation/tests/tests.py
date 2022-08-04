707acf1ccf48cfb6bc46eac13315244c63e26a25
ogulcansarioglu <ogulcansarioglu1@gmail.com>
2022-08-04 03:54:00 +0100

    new test cases

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,11 +1,17 @@
+from translator import english_to_french, french_to_english
+import unittest
 
+class testentofr(unittest.TestCase):
 
-def notNullFrench(self):
-    self.assertIsNotNone(french_to_english("Bonjour"))
-def notNullEnglish(self):
-    self.assertIsNotNone(english_to_french("Hello"))
-def frenchToEnglishTest(self):
-    assert english_to_french("Hello") == "Bonjour"
-def englishtoFrenchTest(self):
-    assert french_to_english("Bonjour") == "Hello"
+    def test_eng_fr(self):
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertEqual(english_to_french("Love"), "Aimer")
+        self.assertEqual(english_to_french("Empty"), " ")
+        self.assertNotEqual(english_to_french("Day"), "")
+    def test_fr_eng(self):
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertEqual(french_to_english("Aimer"), "Love")
+        self.assertEqual(french_to_english("Empty"), "vide")
+        self.assertNotEqual(french_to_english("Journée"), "")
+
     
